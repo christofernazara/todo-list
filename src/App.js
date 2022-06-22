b15b649e224f3b66963c299c1d59b5627b4f432d
christofernazara <christofernazara5@gmail.com>
2022-06-22 17:56:42 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import {useState} from 'react';
-
 function App() {
 
   const [todolist, setTodolist] = useState([])
--- src/index.js
+++ src/index.js
@@ -3,15 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { HashRouter } from "react-router-dom";
+import { HashRouter as Router} from 'react-router-dom';
 
- ReactDOM.render(
-  <React.StrictMode>
-    <HashRouter>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Router>
     <App />
-    </HashRouter> 
-    
-  </React.StrictMode>
+  </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
